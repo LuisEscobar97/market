145aa440a9f72bc779869f595905755fb0eb0f4d
Luis Escobar <luisitoo366@gmail.com>
2021-11-04 21:08:22 -0600

    Third commit

---
+++ src/main/java/com/example/proyecto2/demo/MarketApplication.java
@@ -0,0 +1,14 @@
+package com.example.proyecto2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketApplication.class, args);
+		String a;
+	}
+
+}
---
+++ src/test/java/com/example/proyecto2/demo/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
