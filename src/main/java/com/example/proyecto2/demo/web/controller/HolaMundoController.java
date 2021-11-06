311c5fae1004dd961b57dee0ec14169e965b37f7
Luis Escobar <luisitoo366@gmail.com>
2021-11-05 21:34:29 -0600

    add postgresql conection

--- src/main/java/com/example/proyecto2/demo/web/controller/HolaMundoController.java
+++ src/main/java/com/example/proyecto2/demo/web/controller/HolaMundoController.java
@@ -1,4 +1,4 @@
-package com.example.proyecto2.demo;
+package com.example.proyecto2.demo.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
