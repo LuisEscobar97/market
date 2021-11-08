e49144e97cddb4ece9ed20bfc670b932eb0b1c4d
Luis Escobar <luisitoo366@gmail.com>
2021-11-07 20:35:37 -0600

    add logic to ER

---
+++ src/main/java/com/example/proyecto2/demo/persistence/entity/Categoria.java
@@ -0,0 +1,60 @@
+package com.example.proyecto2.demo.persistence.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+
+    private String descriocion;
+
+    private Boolean estado;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Producto>productos;
+
+    public Categoria(Integer idCategoria, String descriocion, Boolean estado) {
+        this.idCategoria = idCategoria;
+        this.descriocion = descriocion;
+        this.estado = estado;
+    }
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescriocion() {
+        return descriocion;
+    }
+
+    public void setDescriocion(String descriocion) {
+        this.descriocion = descriocion;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "idCategoria=" + idCategoria +
+                ", descriocion='" + descriocion + '\'' +
+                ", estado=" + estado +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/proyecto2/demo/persistence/entity/Cliente.java
@@ -0,0 +1,104 @@
+package com.example.proyecto2.demo.persistence.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="clientes")
+public class Cliente {
+
+    @Id
+    private String id;
+
+    private  String nombre;
+
+    private String apellidos;
+
+    private Long celular;
+
+    private String direccion;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Compra> compras;
+
+    @Column(name = "correo_electronico")
+    private String correoElectronico;
+
+    public Cliente(String id, String nombre, String apellidos, Long celular, String direccion, String correoElectronico) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.celular = celular;
+        this.direccion = direccion;
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Long getCelular() {
+        return celular;
+    }
+
+    public void setCelular(Long celular) {
+        this.celular = celular;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+    public List<Compra> getCompras() {
+        return compras;
+    }
+
+    public void setCompras(List<Compra> compras) {
+        this.compras = compras;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id='" + id + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", celular=" + celular +
+                ", direccion='" + direccion + '\'' +
+                ", compras=" + compras +
+                ", correoElectronico='" + correoElectronico + '\'' +
+                '}';
+    }
+}
