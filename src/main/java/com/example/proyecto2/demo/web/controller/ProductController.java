4cb561636f6a83f77cf26bb224dfba6c37e1ba80
Luis Escobar <luisitoo366@gmail.com>
2021-11-16 21:03:47 -0600

    adding swagger to the project

--- src/main/java/com/example/proyecto2/demo/web/controller/ProductController.java
+++ src/main/java/com/example/proyecto2/demo/web/controller/ProductController.java
@@ -2,7 +2,13 @@ package com.example.proyecto2.demo.web.controller;
 
 import com.example.proyecto2.demo.domain.Product;
 import com.example.proyecto2.demo.domain.service.ProductService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,12 +22,19 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/all")
-    public List<Product> getAll(){
-        return productService.getAll();
+    @ApiOperation("Get all supermarket products")
+    @ApiResponse(code = 200,message = "OK")
+    public ResponseEntity<List<Product>> getAll(){
+        return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
     @GetMapping("/{id}")
-    public Optional<Product> getProduct(@PathVariable("id") int productId){
-        return productService.getProduct(productId);
+    @ApiOperation("Search a product with an ID")
+    @ApiResponses({
+            @ApiResponse(code = 200,message = "OK"),
+            @ApiResponse(code = 400, message = "Not Found")}
+    )
+    public ResponseEntity<Product> getProduct(@ApiParam(value = "The product´s id", required = true,example = "30") @PathVariable("id") int productId){
+        return productService.getProduct(productId).map(product -> new ResponseEntity<>(product,HttpStatus.OK)).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
     @GetMapping("/categeory/{id}")
     public Optional<List<Product>> getByCategory(@PathVariable("id")int categoryID){
